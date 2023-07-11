115cf84a65ae43648ed1b00584501dea424b1cac
Marc Ouellet <marc.ouellet2@gmail.com>
2023-07-11 18:23:11 -0400

    fix delays

--- lib/config/appsettings.dart
+++ lib/config/appsettings.dart
@@ -3,8 +3,7 @@ class AppSettings {
   static const String bleCharacteristicsUUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
   static const String bleDeviceName = "ESP32TM Pool"; 
   static const String bleDeviceNamePrefix = "ESP32TM";   
-  static const int bleDisconnectFromDeviceDurationSeconds = 15;
   static const int bleDeviceScanDurationSeconds = 5;
   static const int bleLookupForDeviceRetryDelaySeconds = 5;
-  static const int bleLookupForDeviceRetriesMaximum = 6;
+  static const int bleLookupForDeviceRetriesMaximum = 12;
 }
